6ab6bbf1486fcee814ea5f843b9a2d39dfbf1852
Mohamad Hadi Salam <51259142+evereadydragon@users.noreply.github.com>
2019-06-25 22:05:52 +0300

    Update TwoThermal.cpp

--- TwoThermal.cpp
+++ TwoThermal.cpp
@@ -2,6 +2,8 @@
 //Reading OpenFOAM polyMesh + Two-Volume Thermal Conduction Solver
 //Mohamad Hadi Salam, American University of Beirut, Mechanical Engineering Department
 //Sunday, February 17th, 2019
+//Licensed June 25, 2019
+//GNU General Public License v3.0 : https://www.gnu.org/licenses/gpl-3.0.en.html
 
 //Compiling: g++ TwoThermal.cpp -o TwoThermal.bin -pthread -std=c++11
 
